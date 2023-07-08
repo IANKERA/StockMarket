67c36481a35fe38395e6834bc3cd4db77f8540cf
IANKERA <123420122+IANKERA@users.noreply.github.com>
2023-07-08 21:35:13 +0000

    await plus map()

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,5 +1,10 @@
 import axios from "axios"
 
+const TOKEN = "ciknv61r01qk492qs3t0ciknv61r01qk492qs3tg"
+
 export default axios.create({
-  baseURL: "https://finnhub.io/api/v1"
+  baseURL: "https://finnhub.io/api/v1",
+  params:{
+  token: TOKEN
+  }
 })
--- src/componets/StockList.jsx
+++ src/componets/StockList.jsx
@@ -1,6 +1,39 @@
 import{useState, useEffect} from "react"
+import finnHub from "../apis/finnHub"
 
 export const StockList = () => {
+  const[stock , setStock] = useState()
  const [watchList, setWatch] = useState(["GOOGL", "MSFT", "AMZN"])
+
+  useEffect(() => {
+    let isMounted = true
+    const fetchData = async () => {
+      const responses = []
+      try {
+      
+       const responses = await Promise.all(watchList.map((s)           => {
+         return finnHub.get("/quote", {
+           params: {
+             symbol: stock
+           }
+         })
+       }))
+      
+       
+                        
+        console.log(responses)
+        if (isMounted) {
+          setStock(responses)
+        }
+      
+      }catch (err) {
+        
+      }
+    }
+    fetchData()
+
+    return () => (isMounted = false)
+    
+  },[])
   return <div>StockList</div>
 }
